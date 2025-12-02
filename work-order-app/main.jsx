54a9ef219ff07f569e029dd0054800e8dce63a9b
brettspolen <brettspolen@gmail.com>
2025-12-02 07:24:41 -0700

    Add files via upload

---
+++ work-order-app/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import WorkOrderApp from './WorkOrderApp'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WorkOrderApp />
+  </React.StrictMode>,
+)
